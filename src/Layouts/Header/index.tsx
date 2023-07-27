a1510b71099c0da5491d9f00b351b17fae581dfb
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-07-27 19:54:23 +0300

    add navbar elements

--- src/Layouts/Header/index.tsx
+++ src/Layouts/Header/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function index() {
   return (
-    <div>index</div>
-  )
+    <nav className="absolute top-2.5 w-full">
+      <div className="w-2/4 m-auto p-2 rounded-lg bg-secondary-700">
+        <ul className="flex flex-row flex-nowrap space-x-4">
+          <a href="">
+            <li className="p-1.5 rounded-md hover:text-primary-500 hover:bg-secondary-800 transition-all">
+              Home
+            </li>
+          </a>
+          <a href="">
+            <li className="p-1.5 rounded-md hover:text-primary-500 hover:bg-secondary-800 transition-all">
+              Dashboard
+            </li>
+          </a>
+          <a href="">
+            <li className="p-1.5 rounded-md hover:text-primary-500 hover:bg-secondary-800 transition-all">
+              Report Form
+            </li>
+          </a>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default index
+export default index;
