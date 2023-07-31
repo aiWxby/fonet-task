bf0b8f7870aa8190d6968aacdc8c064815c4bc99
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-07-31 21:21:26 +0300

    refactor: remove unnecessary element

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,12 +3,10 @@ import Description from "../../components/Home/Description";
 
 function HomePage() {
   return (
-    <div className="w-2/4 mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <Banner />
-        <hr className="w-full border-t border-dashed border-primary-500/50 my-8" />
-        <Description />
-      </div>
+    <div className="flex flex-col justify-center items-center">
+      <Banner />
+      <hr className="w-full border-t border-dashed border-primary-500/50 my-8" />
+      <Description />
     </div>
   );
 }
