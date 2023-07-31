d73b9983324bc498180ad8282b12d6b6474b12c0
Ertuğrul Elibol <ai.wxby@gmail.com>
2023-07-31 21:42:11 +0300

    refactor: added error props & elements

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,34 +10,46 @@ type InputPropTypes = {
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value?: string | number;
+  errors?: string | undefined;
+  touched?: boolean | undefined;
 };
 
 function Input({
   id,
   wrapperClasses,
-  labelClasses,
   label,
+  labelClasses,
   type,
   inputClasses,
   placeholder,
   onChange,
   value,
+  errors,
+  touched,
 }: InputPropTypes) {
   return (
-    <div className={`${wrapperClasses}`}>
-      {label && (
-        <label htmlFor={id} className={`${labelClasses}`}>
-          {label}
-        </label>
-      )}
-      <input
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        className={`${inputClasses}`}
-        onChange={onChange}
-        value={value}
-      />
+    <div className="space-y-2">
+      <div className={`${wrapperClasses}`}>
+        {label && (
+          <label htmlFor={id} className={`${labelClasses}`}>
+            {label}
+          </label>
+        )}
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          className={`${inputClasses}`}
+          onChange={onChange}
+          value={value}
+        />
+      </div>
+      {errors &&
+      touched ? (
+        <p className="text-red-500">
+          {errors}
+        </p>
+      ) : null}
     </div>
   );
 }
